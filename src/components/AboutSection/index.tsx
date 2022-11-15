a17276af17d8c5ea123be6ca170aaf2b904db7c2
thallesyam <thallesyam@gmail.com>
2022-11-15 20:54:10 -0300

    feat: finish about section

---
+++ src/components/AboutSection/index.tsx
@@ -0,0 +1,43 @@
+import { About } from '../../assets/About'
+import { Css } from '../../assets/Css'
+import { React } from '../../assets/React'
+import { Typescript } from '../../assets/Typescript'
+
+import style from './styles.module.scss'
+
+export function AboutSection() {
+  return (
+    <section className={style.about}>
+      <section>
+        <div className={style.icon_container}>
+          <About />
+
+          <div className={`${style.tech_icon} ${style.first_icon}`}>
+            <Typescript />
+          </div>
+          <div className={`${style.tech_icon} ${style.second_icon}`}>
+            <Css />
+          </div>
+          <div className={`${style.tech_icon} ${style.third_icon}`}>
+            <React />
+          </div>
+        </div>
+
+        <div className={style.text_container}>
+          <h2>
+            Quem sou <span>.</span>
+          </h2>
+
+          <p>
+            Meu nome é Thalles, sou apaixonado por construir soluções úteis que
+            ajudem a criar experiências únicas e marcantes para os usuários. Sou
+            um desenvolvedor frontend entusiasta da stack Javascript. Amo
+            estudar sobre arquitetura de software, design patterns, paradigmas
+            de programação e utilizo todas essas habilidades e competências para
+            resolver problemas através da tecnologia.
+          </p>
+        </div>
+      </section>
+    </section>
+  )
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,36 +1,12 @@
-import { WhatsappLogo, LinkedinLogo } from 'phosphor-react'
-
-import { Header } from '../components/Header'
-
-import style from '../styles/pages/home.module.scss'
+import { AboutSection } from '../components/AboutSection'
+import { StartSection } from '../components/StartSection'
 
 export default function Home() {
   return (
     <main>
-      <Header />
+      <StartSection />
 
-      <section className={style.title_section}>
-        <h1>
-          Navegando pelo mundo da programação e ajudando empresas a construírem
-          <span>jornadas inesquecíveis.</span>
-        </h1>
-
-        <p>
-          Sempre priorizando a experiência do usuário e trazendo uma visão que
-          vai destacar o seu produto no mercado.
-        </p>
-
-        <div>
-          <button className={style.btn_primary}>
-            <WhatsappLogo size={16} />
-            <p>Whatsapp</p>
-          </button>
-          <button className={style.btn_secondary}>
-            <LinkedinLogo size={16} />
-            <p>Linkedin</p>
-          </button>
-        </div>
-      </section>
+      <AboutSection />
     </main>
   )
 }
