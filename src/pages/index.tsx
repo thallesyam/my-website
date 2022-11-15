c127e1ec917409035ee0858bc26f938b16ef97f7
thallesyam <thallesyam@gmail.com>
2022-11-15 16:42:31 -0300

    feat: create initial cta

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,36 @@
+import { WhatsappLogo, LinkedinLogo } from 'phosphor-react'
+
 import { Header } from '../components/Header'
 
+import style from '../styles/pages/home.module.scss'
+
 export default function Home() {
-  return <Header />
+  return (
+    <main>
+      <Header />
+
+      <section className={style.title_section}>
+        <h1>
+          Navegando pelo mundo da programação e ajudando empresas a construírem
+          <span>jornadas inesquecíveis.</span>
+        </h1>
+
+        <p>
+          Sempre priorizando a experiência do usuário e trazendo uma visão que
+          vai destacar o seu produto no mercado.
+        </p>
+
+        <div>
+          <button className={style.btn_primary}>
+            <WhatsappLogo size={16} />
+            <p>Whatsapp</p>
+          </button>
+          <button className={style.btn_secondary}>
+            <LinkedinLogo size={16} />
+            <p>Linkedin</p>
+          </button>
+        </div>
+      </section>
+    </main>
+  )
 }
